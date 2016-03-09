0b52055dcb44671cc6fa29ec01b417b13f4a1f1e
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-03-09 17:01:40 +0900

    Add flags_into_stream()

--- getopt.hpp
+++ getopt.hpp
@@ -1,6 +1,7 @@
 // -*- mode: c++; coding: utf-8 -*-
 #pragma once
 
+#include <ostream>
 #include <sstream>
 #include <boost/program_options.hpp>
 
@@ -8,44 +9,51 @@
 namespace wtl {
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
-inline std::string flags_into_string(
+inline std::ostream& flags_into_stream(
+            std::ostream& ost,
             const boost::program_options::options_description& desc,
             const boost::program_options::variables_map& vm) {
-
-    std::ostringstream oss;
-    oss.precision(16);
     for (const auto shared_ptr_opt: desc.options()) {
-        std::string long_name(shared_ptr_opt.get()->long_name());
-        boost::any value(vm[long_name].value());
+        std::string long_name = shared_ptr_opt.get()->long_name();
+        boost::any value = vm[long_name].value();
         if (!vm.count(long_name)) {continue;}
         if (value.type() == typeid(std::string)
             && boost::any_cast<std::string>(value).empty()) {
-            oss << '#';
+            ost << '#';
         }
-        oss << long_name << " = ";
+        ost << long_name << " = ";
         if (value.type() == typeid(int)) {
-            oss << boost::any_cast<int>(value);
+            ost << boost::any_cast<int>(value);
         }
         else if (value.type() == typeid(unsigned int)) {
-            oss << boost::any_cast<unsigned int>(value);
+            ost << boost::any_cast<unsigned int>(value);
         }
         else if (value.type() == typeid(size_t)) {
-            oss << boost::any_cast<size_t>(value);
+            ost << boost::any_cast<size_t>(value);
         }
         else if (value.type() == typeid(double)) {
-            oss << boost::any_cast<double>(value);
+            ost << boost::any_cast<double>(value);
         }
         else if (value.type() == typeid(bool)) {
-            oss << boost::any_cast<bool>(value);
+            ost << boost::any_cast<bool>(value);
         }
         else if (value.type() == typeid(std::string)) {
-            oss << '"' << boost::any_cast<std::string>(value) << '"';
+            ost << '"' << boost::any_cast<std::string>(value) << '"';
         }
         else {
-            oss << boost::any_cast<unsigned long>(value);
+            ost << boost::any_cast<unsigned long>(value);
         }
-        oss << '\n';
+        ost << '\n';
     }
+    return ost;
+}
+
+inline std::string flags_into_string(
+            const boost::program_options::options_description& desc,
+            const boost::program_options::variables_map& vm) {
+    std::ostringstream oss;
+    oss.precision(std::numeric_limits<double>::max_digits10);
+    flags_into_stream(oss, desc, vm);
     return oss.str();
 }
 
