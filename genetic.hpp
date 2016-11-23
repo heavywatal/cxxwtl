0e7627d4c7e82c249b2b8f4129d50dc8048bbbdb
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-11-24 01:31:09 +0900

    Add low level functions to genetic.hpp

--- genetic.hpp
+++ genetic.hpp
@@ -10,14 +10,47 @@
 namespace wtl {
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
+//! Make upper bounds from fitness vector
+inline std::vector<double> make_ubounds(std::vector<double> v) {
+    std::partial_sum(v.begin(), v.end(), v.begin());
+    return v;
+}
+
+inline size_t throw_dart(const std::vector<double>& ubounds, const double dart) {
+    size_t idx = 0;
+    while (ubounds[idx] < dart) {++idx;}
+    return idx;
+}
+
 template <class RNG> inline
-std::vector<size_t> roulette_select(const std::vector<double>& fitnesses,
-                                    RNG& rng,
-                                    const size_t elites=0) {
-    // make a roulette
-    const size_t pop_size = fitnesses.size();
-    std::vector<double> upper_bounds(pop_size);
-    std::partial_sum(begin(fitnesses), end(fitnesses), begin(upper_bounds));
+size_t roulette_select(const std::vector<double>& fitnesses, RNG& rng) {
+    const std::vector<double> ubounds = make_ubounds(fitnesses);
+    std::uniform_real_distribution<double> uniform(0.0, ubounds.back());
+    return throw_dart(make_ubounds(fitnesses), uniform(rng));
+}
+
+template <class RNG> inline
+std::vector<size_t> roulette_select(
+    const std::vector<double>& fitnesses, const size_t n, RNG& rng) {
+
+    const std::vector<double> ubounds = make_ubounds(fitnesses);
+    std::uniform_real_distribution<double> uniform(0.0, ubounds.back());
+    std::vector<size_t> selected_idx;
+    selected_idx.reserve(n);
+    for (size_t i=0; i<n; ++i) {
+        selected_idx.push_back(throw_dart(ubounds, uniform(rng)));
+    }
+    return selected_idx;
+}
+
+template <class RNG> inline
+std::vector<size_t> roulette_select(
+    const std::vector<double>& fitnesses,
+    RNG& rng,
+    const size_t pop_size,
+    const size_t elites=0) {
+
+    const std::vector<double> upper_bounds = make_ubounds(fitnesses);
     const double sum_fitness = upper_bounds.back();
 
     std::vector<size_t> candidates(pop_size);
@@ -51,10 +84,7 @@ std::vector<size_t> roulette_select(const std::vector<double>& fitnesses,
     else {
         std::uniform_real_distribution<double> uniform(0.0, sum_fitness);
         while (children.size() < pop_size) {
-            size_t i = 0;
-            const double dart(uniform(rng));
-            while (upper_bounds[i] < dart) {++i;}
-            children.push_back(i);
+            children.push_back(throw_dart(upper_bounds, uniform(rng)));
         }
     }
     return children;
