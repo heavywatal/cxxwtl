39436dd68985e60986f8bd841ff4154dce8fc1a6
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-01-26 20:48:05 +0900

    Rename variables

--- itertools.hpp
+++ itertools.hpp
@@ -19,55 +19,55 @@ template <class value_type>
 class Product {
   private:
     typedef boost::coroutines2::coroutine<value_type> coro_t;
-    const std::vector<value_type> columns_;
+    const std::vector<value_type> axes_;
     value_type value_;
     typedef typename std::remove_const<decltype(value_.size())>::type size_type;
-    size_type col_;
+    size_type pos_;
     boost::multiprecision::cpp_int cnt_ = 0;
   public:
     Product() = delete;
-    explicit Product(const std::vector<value_type>& columns):
-        columns_(columns),
-        value_(columns_.size()),
-        col_(columns_.size()) {}
+    explicit Product(const std::vector<value_type>& axes):
+        axes_(axes),
+        value_(axes_.size()),
+        pos_(axes_.size()) {}
 
     typename coro_t::pull_type operator()(const size_type start=0) {
         return typename coro_t::pull_type([this,start](typename coro_t::push_type& yield){source(yield, start);});
     }
 
-    void reset() {col_ = columns_.size();}
+    void reset() {pos_ = axes_.size();}
     boost::multiprecision::cpp_int count() const {return cnt_;}
     boost::multiprecision::cpp_int count_max() const {
         boost::multiprecision::cpp_int i = 1;
-        for (const auto& c: columns_) {i *= c.size();}
+        for (const auto& c: axes_) {i *= c.size();}
         return i;
     }
 
   private:
     void source(typename coro_t::push_type& yield, const size_type start) {
-        if (--col_ > 0) {
-            const size_type n = columns_[col_].size();
+        if (--pos_ > 0) {
+            const size_type n = axes_[pos_].size();
             for (size_type i=0; i<n; ++i) {
-                value_[col_] = columns_[col_][i];
+                value_[pos_] = axes_[pos_][i];
                 source(yield, start);
             }
-            ++col_;
+            ++pos_;
         } else {
-            const size_type n = columns_[col_].size();
+            const size_type n = axes_[pos_].size();
             for (size_type i=0; i<n; ++i) {
-                value_[col_] = columns_[col_][i];
+                value_[pos_] = axes_[pos_][i];
                 if (cnt_++ >= start) {
                     yield(value_type(value_));
                 }
             }
-            ++col_;
+            ++pos_;
         }
     }
 };
 
 template <class value_type>
-Product<value_type> product(const std::vector<value_type>& columns) {
-    return Product<value_type>(columns);
+Product<value_type> product(const std::vector<value_type>& axes) {
+    return Product<value_type>(axes);
 }
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
@@ -77,8 +77,8 @@ class Simplex {
     typedef boost::coroutines2::coroutine<value_type> coro_t;
     Simplex() = delete;
   public:
-    explicit Simplex(const std::vector<value_type>& columns, const double sum=1.0):
-        product_(columns), sum_(sum) {}
+    explicit Simplex(const std::vector<value_type>& axes, const double sum=1.0):
+        product_(axes), sum_(sum) {}
 
     typename coro_t::pull_type operator()(void) {
         return typename coro_t::pull_type([this](typename coro_t::push_type& yield){source(yield);});
@@ -108,8 +108,8 @@ class Simplex {
 };
 
 template <class value_type>
-Simplex<value_type> simplex(const std::vector<value_type>& columns, const double sum=1.0) {
-    return Simplex<value_type>(columns, sum);
+Simplex<value_type> simplex(const std::vector<value_type>& axes, const double sum=1.0) {
+    return Simplex<value_type>(axes, sum);
 }
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
