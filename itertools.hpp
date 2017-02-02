64df933cc41bd48fbe0bf223be5b946ef489ae8a
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-02-02 21:41:29 +0900

    Create a new base class itertools::Generator

--- itertools.hpp
+++ itertools.hpp
@@ -16,58 +16,119 @@ namespace wtl { namespace itertools {
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
 template <class value_type>
-class Product {
-  private:
-    typedef boost::coroutines2::coroutine<value_type> coro_t;
-    const std::vector<value_type> axes_;
-    value_type value_;
-    typedef typename std::remove_const<decltype(value_.size())>::type size_type;
-    size_type pos_;
-    boost::multiprecision::cpp_int cnt_ = 0;
+class Generator {
   public:
+    typedef boost::coroutines2::coroutine<value_type> coro_t;
+    typedef boost::multiprecision::cpp_int size_type;
+    typedef typename std::remove_const<decltype(value_type().size())>::type value_size_t;
+
+    Generator() = default;
+    virtual ~Generator() = default;
+
+    size_type count() const {return cnt_;}
+    virtual size_type max_count() const = 0;
+    typename coro_t::pull_type operator()(const size_type start=0) {
+        return typename coro_t::pull_type([this,start](typename coro_t::push_type& yield){source(yield, start);});
+    }
+  protected:
+    size_type cnt_ = 0;
+    virtual void source(typename coro_t::push_type& yield, const size_type start) = 0;
+};
+
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
+
+template <class value_type>
+class Product final: public Generator<value_type> {
+  public:
+    using typename Generator<value_type>::coro_t;
+    using typename Generator<value_type>::size_type;
+    using typename Generator<value_type>::value_size_t;
     Product() = delete;
     explicit Product(const std::vector<value_type>& axes):
+        Generator<value_type>(),
         axes_(axes),
         value_(axes_.size()),
         pos_(axes_.size()) {}
-
-    typename coro_t::pull_type operator()(const boost::multiprecision::cpp_int start=0) {
-        return typename coro_t::pull_type([this,start](typename coro_t::push_type& yield){source(yield, start);});
-    }
+    ~Product() = default;
 
     void reset() {pos_ = axes_.size();}
-    boost::multiprecision::cpp_int count() const {return cnt_;}
-    boost::multiprecision::cpp_int max_count() const {
-        boost::multiprecision::cpp_int i = 1;
-        for (const auto& c: axes_) {i *= c.size();}
-        return i;
+    virtual size_type max_count() const override {
+        size_type n = 1;
+        for (const auto& c: axes_) {n *= c.size();}
+        return n;
     }
 
   private:
-    void source(typename coro_t::push_type& yield, const boost::multiprecision::cpp_int start) {
+    virtual void source(typename coro_t::push_type& yield, const size_type start) override {
         if (--pos_ > 0) {
-            const size_type n = axes_[pos_].size();
-            for (size_type i=0; i<n; ++i) {
+            const value_size_t n = axes_[pos_].size();
+            for (value_size_t i=0; i<n; ++i) {
                 value_[pos_] = axes_[pos_][i];
                 source(yield, start);
             }
             ++pos_;
         } else {
-            const size_type n = axes_[pos_].size();
-            for (size_type i=0; i<n; ++i) {
+            const value_size_t n = axes_[pos_].size();
+            for (value_size_t i=0; i<n; ++i) {
                 value_[pos_] = axes_[pos_][i];
-                if (cnt_++ >= start) {
+                if (this->cnt_++ >= start) {
                     yield(value_type(value_));
                 }
             }
             ++pos_;
         }
     }
+    const std::vector<value_type> axes_;
+    value_type value_;
+    value_size_t pos_;
 };
 
 template <class value_type>
 Product<value_type> product(const std::vector<value_type>& axes) {
     return Product<value_type>(axes);
+}
+
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
+
+template <class value_type>
+class UniAxis final: public Generator<value_type> {
+  public:
+    using typename Generator<value_type>::coro_t;
+    using typename Generator<value_type>::size_type;
+    using typename Generator<value_type>::value_size_t;
+    UniAxis() = delete;
+    UniAxis(const std::vector<value_type>& axes, const value_type& center):
+      Generator<value_type>(),
+      axes_(axes), center_(center) {}
+    ~UniAxis() = default;
+
+    virtual size_type max_count() const override {
+        size_type n = 0;
+        for (const auto& c: axes_) {n += c.size();}
+        return n;
+    }
+
+  private:
+    virtual void source(typename coro_t::push_type& yield, const size_type start) override {
+        for (size_t i=0; i<axes_.size(); ++i) {
+            auto value = center_;
+            const auto axis = axes_[i];
+            const auto l = axis.size();
+            for (value_size_t j=0; j<l; ++j) {
+                value[i] = axis[j];
+                if (this->cnt_++ >= start) {
+                    yield(value_type(value));
+                }
+            }
+        }
+    }
+    const std::vector<value_type> axes_;
+    const value_type center_;
+};
+
+template <class value_type>
+UniAxis<value_type> uniaxis(const std::vector<value_type>& axes, const value_type& center) {
+    return UniAxis<value_type>(axes, center);
 }
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
