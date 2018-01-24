29bb430f9a8086c968abf7e78b1f1d9531b9b53e
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-24 23:01:53 +0900

    :white_check_mark: Add test/math.cpp

---
+++ test/math.cpp
@@ -0,0 +1,13 @@
+#include "math.hpp"
+#include "exception.hpp"
+
+int main() {
+    WTL_ASSERT(wtl::pow(2u, 0u) == 1u);
+    WTL_ASSERT(wtl::pow(2u, 1u) == 2u);
+    WTL_ASSERT(wtl::pow(2u, 8u) == 256u);
+    WTL_ASSERT(wtl::factorial(0u) == 1u);
+    WTL_ASSERT(wtl::factorial(5u) == 120u);
+    WTL_ASSERT(wtl::permut(5u, 2u) == 20u);
+    WTL_ASSERT(wtl::choose(5u, 2u) == 10u);
+    return 0;
+}
