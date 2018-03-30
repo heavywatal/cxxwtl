047d19b84870a8c5cc1d1ca5cd0e637842f7f9a6
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-03-30 14:07:34 +0900

    :white_check_mark: Add test for generate_canonical()

--- test/random.cpp
+++ test/random.cpp
@@ -6,7 +6,16 @@
 int main() {
     for (size_t i=0; i<6u; ++i) {
         double x = wtl::generate_canonical(wtl::mt64());
-        std::cout << x << ", ";
-        WTL_ASSERT(0.0 < x && x <= 1.0);
+        std::cout << x << " ";
+        WTL_ASSERT(0.0 <= x && x < 1.0);
     }
+    std::cout << "\n";
+    wtl::bits64_t min_0x3ff{uint64_t{0x3ff0'0000'0000'0000u}}; // 1.0
+    wtl::bits64_t max_0x3ff{uint64_t{0x3fff'ffff'ffff'ffffu}}; // 1.999...
+    WTL_ASSERT((min_0x3ff.as_double - 1.0) == 0.0);
+    WTL_ASSERT((max_0x3ff.as_double - 1.0) < 1.0);
+    std::cout.precision(15);
+    std::cout << std::scientific
+              << min_0x3ff.as_double - 1.0 << "\n"
+              << max_0x3ff.as_double - 1.0 << "\n";
 }
