f7670e5a116c882eb7ee4484ba042893e8cab9fa
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-15 15:16:13 +0900

    :white_check_mark: GCC cannot catch std::ios_base::failure

--- test/zlib.cpp
+++ test/zlib.cpp
@@ -2,27 +2,38 @@
 #include "exception.hpp"
 
 #include <cerrno>
-#include <iostream>
+#include <sstream>
+
+void write_read() {
+    std::ostringstream original;
+    {
+        std::ifstream ifs("../../test/zlib.cpp");
+        original << ifs.rdbuf();
+    }
+    wtl::zlib::ofstream("zlib.cpp.gz") << original.str();
+    std::ostringstream archived;
+    {
+        wtl::zlib::ifstream ifs("zlib.cpp.gz");
+        archived << ifs.rdbuf();
+    }
+    WTL_ASSERT(original.str() == archived.str());
+}
+
+void exception() {
+    bool exception_thrown = false;
+    try {
+        wtl::zlib::ifstream ifs("noexist.gz");
+    } catch (const std::ios_base::failure& e) {
+        exception_thrown = true;
+    } catch (const std::exception& e) {// gcc bug
+        exception_thrown = true;
+    }
+    WTL_ASSERT(exception_thrown);
+    WTL_ASSERT(errno == ENOENT);
+}
 
 int main() {
-    {
-      std::ifstream ifs("../../test/zlib.cpp");
-      wtl::zlib::ofstream ofs("zlib.cpp.gz");
-      ofs << ifs.rdbuf();
-    }
-    {
-      wtl::zlib::ifstream ifs("zlib.cpp.gz");
-      std::ostringstream oss;
-      oss << ifs.rdbuf();
-      std::cout << oss.str();
-    }
-    bool exception_handled = false;
-    try {
-      wtl::zlib::ifstream ifs("noexist.gz");
-    } catch (std::ios_base::failure& e) {
-      WTL_ASSERT(errno == ENOENT);
-      exception_handled = true;
-    }
-    WTL_ASSERT(exception_handled);
+    write_read();
+    exception();
     return 0;
 }
